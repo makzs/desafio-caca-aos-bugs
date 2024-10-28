1a63198570b57810a4ce5d001dea02f5d68fa828
makzs <erikmakiak@gmail.com>
2024-10-28 14:52:09 -0300

    BUG 03: Categoria não Lista -Resolvido

--- bugs/Dima.Web/Pages/Categories/List.razor.cs
+++ bugs/Dima.Web/Pages/Categories/List.razor.cs
@@ -11,7 +11,7 @@ public partial class ListCategoriesPage : ComponentBase
     #region Properties
 
     public bool IsBusy { get; set; } = false;
-    public List<Category> Categories { get; set; }
+    public List<Category> Categories { get; set; } = new List<Category>();
     public string SearchTerm { get; set; } = string.Empty;
 
     #endregion
@@ -38,7 +38,7 @@ public partial class ListCategoriesPage : ComponentBase
         {
             var request = new GetAllCategoriesRequest();
             var result = await Handler.GetAllAsync(request);
-            if (result.IsSuccess)
+            if (result.IsSuccess && result.Data is not null)
                 Categories.AddRange(result.Data);
         }
         catch (Exception ex)
